876be4a53a7e59d58f10d3f261ced9cb8e13a010
ErKable <103565122+ErKable@users.noreply.github.com>
2023-06-28 15:33:21 +0200

    Add files via upload

---
+++ contracts/TokenStaking.sol
@@ -0,0 +1,108 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./rewartdToken.sol";
+import "./depositToken.sol";
+
+contract Staking is Ownable{
+
+    enum rewardTier{
+        Low,
+        Medium,
+        High
+    }
+
+    struct userInfo {
+        uint256 depositBlock;
+        uint256 lastClaimBlock;
+        uint256 depositedAmount;
+        uint256 lockedUntil;
+        rewardTier userRewardTier;
+    }
+
+    struct rewardPerBlock {
+        uint256 tierOne;
+        uint256 tierTwo;
+        uint256 tierThree;
+    }
+
+    depositToken public dToken;
+    rewardToken public rToken;
+    uint256 public rTokenBalance;
+    uint256 public minimumDepositTime;
+    rewardPerBlock public rewards;
+    mapping(address => bool) public hasDeposited;
+    mapping(address => userInfo) public stakedUser;
+
+    event StakingCreated(uint256 indexed at);
+    event PoolFunded(uint256 indexed at, uint256 indexed amount);
+    event UserStaked(address indexed user, uint256 indexed depositedAmount, uint256 indexed at);
+
+    constructor(address _tokenToDeposit, address _rewardToken, uint256[] memory _rewardPerBlock, uint256 _minimumDepositTime)  {
+        require(_rewardPerBlock.length > 0, "RPB too short");
+        require(_rewardPerBlock.length < 4, "RPB too long");
+        dToken = depositToken(_tokenToDeposit);
+        rToken = rewardToken(_rewardToken);
+        rewards.tierOne = _rewardPerBlock[0];
+        rewards.tierTwo = _rewardPerBlock[1];
+        rewards.tierThree = _rewardPerBlock[2];
+        minimumDepositTime = _minimumDepositTime;
+        emit StakingCreated(block.timestamp);
+    }
+
+    function fundStaking(uint256 amount) external onlyOwner{
+        require(rToken.balanceOf(msg.sender) > amount - 1, "Not enought balance");
+        rTokenBalance += amount;
+        rToken.transferFrom(msg.sender, address(this), amount);
+        emit PoolFunded(block.timestamp, amount);
+    }
+
+    function deposit(uint256 amount, uint256 depositTime) external {
+        require(dToken.balanceOf(msg.sender) > amount - 1, "Not enought balance");
+        require(depositTime > minimumDepositTime, "Deposit time too low");
+        hasDeposited[msg.sender] = true;
+        (rewardTier tier,) = checkTier(amount);
+        stakedUser[msg.sender] = userInfo(block.number, block.number, amount, block.timestamp + depositTime, tier);
+        dToken.transferFrom(msg.sender, address(this), amount);
+        emit UserStaked(msg.sender, amount, block.timestamp);
+    }
+
+    function claimRewards() public {
+        require(hasDeposited[msg.sender], "Cannot claim if not deposited");
+        uint256 rewardAmount = checkRewards(msg.sender);
+        stakedUser[msg.sender].lastClaimBlock = block.number;
+        rToken.transferFrom(address(this), msg.sender, rewardAmount);
+    }
+
+    function withdraw() external{
+        userInfo memory tempInfo = stakedUser[msg.sender];
+        require(tempInfo.lockedUntil > block.timestamp, "Timelock not expired yet");
+        claimRewards();
+        delete stakedUser[msg.sender];
+        dToken.transfer(msg.sender, tempInfo.depositedAmount);
+    }
+
+    function checkRewards(address _stakedUser) public view returns(uint256 rewardAmount) {
+        userInfo memory tempInfo = stakedUser[_stakedUser];
+        uint256 currentBlock = block.number;
+        uint256 passedBlock = currentBlock - tempInfo.lastClaimBlock;
+        (,uint256 amount) = checkTier(tempInfo.depositedAmount);
+        rewardAmount = passedBlock * amount;
+    }
+
+    function checkTier(uint256 amount) public view returns(rewardTier tier, uint256 tokenPerBlock) {
+        rewardPerBlock memory tempTier = rewards;
+        if(amount < tempTier.tierOne){
+            tier = rewardTier.Low;
+            amount = rewards.tierOne;
+        } else if(amount > tempTier.tierOne && amount < tempTier.tierTwo) {
+            tier = rewardTier.Medium;
+            amount = rewards.tierTwo;
+        } else if (amount > tempTier.tierThree){
+            tier = rewardTier.High;
+            amount = rewards.tierThree;
+        }
+    }
+    
+}
---
+++ test/Staking.test.js
@@ -0,0 +1,73 @@
+const { ethers } = require("hardhat");
+const { expect, assert } = require("chai");
+
+describe(`Simple staking test`, function(){
+
+    let dToken
+    let dTokenAddress
+
+    let rToken
+    let rTokenAddress
+
+    let staking
+    let stakingAddress
+
+    let owner
+    let ownerAddress
+
+    let user = []
+    let userAddress = []
+
+    it(`Test init`, async function(){
+        owner = ethers.provider.getSigner(0)
+        ownerAddress = await owner.getAddress()
+
+        console.log(`Owner address ${ownerAddress}`)
+
+        for(let i = 0; i < 10; i++){
+            user[i] = ethers.provider.getSigner(i+1)
+            userAddress[i] = await user[i].getAddress()
+            console.log(`Temp user ${i + 1} address: ${userAddress[i]}`)
+        }
+    })
+
+    it(`Should deploy deposit token and reward token`, async function(){
+        dToken = await(await(await ethers.getContractFactory('rewardToken', owner)).deploy()).deployed()
+        dTokenAddress = dToken.address 
+        console.log(`Deposit token deployed to: ${dTokenAddress}`)
+
+        rToken = await(await(await ethers.getContractFactory('depositToken', owner)).deploy()).deployed()
+        rTokenAddress = rToken.address
+        console.log(`Reward token deployed to: ${rTokenAddress}`)
+    })
+
+    it(`Should deploy the staking`, async function(){
+        staking = await(await(await ethers.getContractFactory('Staking', owner)).deploy(dTokenAddress, rTokenAddress, [100, 1000, 10000], 86400)).deployed()
+        stakingAddress = staking.address
+        console.log(`Staking deployed to ${stakingAddress}`)
+    })
+
+    it(`Should fund the staking`, async function(){
+        let ownerBalance = await rToken.balanceOf(ownerAddress)
+        console.log(`Reward token owner balance: ${ownerBalance}`)
+        var stakingBalance = await rToken.balanceOf(stakingAddress)
+        console.log(`Staking reward token balance: ${stakingBalance}`)
+
+        let approve = await rToken.approve(stakingAddress, ownerBalance)
+        await approve.wait()
+
+        await staking.fundStaking(ownerBalance)
+        stakingBalance = await rToken.balanceOf(stakingAddress)
+        console.log(`Staking funded with ${stakingBalance} reward tokens`)
+    })
+
+    it(`Should fund users with deposit token`, async function(){
+        for(let i = 0; i < userAddress.length; i++){
+            await dToken.transfer(userAddress[i], '100000000000000000000000')
+            let tempUserBal = await dToken.balanceOf(userAddress[i])
+            console.log(`Temp user ${userAddress[i]} balance: ${tempUserBal}`)
+        }
+    })
+
+
+})
