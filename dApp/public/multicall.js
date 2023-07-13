5da5c9f8a9518fa3f94ae12073bd20cd0f23ac4d
ErKable <103565122+ErKable@users.noreply.github.com>
2023-07-13 12:43:52 +0200

    Delete multicall.js

--- dApp/public/multicall.js
+++
@@ -1,8 +0,0 @@
-import { providers } from '@0xsequence/multicall'
-import { providers as ethersProviders, ethers } from 'ethers'
-import {abi} from './abi'
-
-export async function viewMulticall(){
-    const provider = new providers.MulticallProvider(new ethersProviders.JsonRpcProvider("https://rpc-mumbai.maticvigil.com"))
-    
-}
